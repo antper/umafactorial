695862d6aeb90418b6d15328e8cba3d9ee810757
antper <antoniopcuma@hotmail.com>
2017-03-14 13:57:50 +0100

    recursiva

--- factorial.java
+++ factorial.java
@@ -3,17 +3,16 @@ import java.util.Scanner;
 public class factorial{
 
 	public static long fact (long num){
-		long fac = 1;
-		for(long i=1;i<=num;++i){
-			fac = fac * i;
-		}
-		return fac;
+                if(num<=0){
+		  return 1;
+		}else{
+		 return num*fact(num-1);
 	}
   public static void main(String []args){
     Scanner scanInput = new Scanner (System.in);
     System.out.print("Introduzca un número: ");
     long num = scanInput.nextLong();
-      
+    
     long fac = fact(num);
 
     }
