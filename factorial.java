ea8c65e52804efcf39564afa255f2dcd0967b18b
antper <antoniopcuma@hotmail.com>
2017-03-14 16:24:32 +0100

    optimizacion

--- factorial.java
+++ factorial.java
@@ -3,21 +3,30 @@ import java.util.Scanner;
 public class factorial{
 
 	public static long fact (long num){
-		long fac = 1;
-		for(long i=1;i<=num;++i){
-			fac = fac * i;
-		}
-		return fac;
+                if(num<=1){
+		  return 1;
+		}else{
+		 return num*fact(num-1);
 	}
   public static void main(String []args){
     Scanner scanInput = new Scanner (System.in);
     System.out.print("Introduzca un número: ");
+<<<<<<< HEAD
     
     if(num<0){
 	System.out.print("El factorial no esta definido" + " para mantener numeros negativos");
     }else{
 	long fac = fact(num);
 	System.out.println("El factorial de"+num+"es "+fac);
+=======
+    long num = scanInput.nextLong();
+    
+    long fac = fact(num);
+
+    }
+    System.out.println("El factorial de "+num+"es"+fac);
+    scanInput.close();
+>>>>>>> recursivo
   }
   scanInput.close();
 }
